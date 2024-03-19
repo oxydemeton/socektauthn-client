52c6f2cbcda4b01c3bb805a330deb19965c2dd51
oxydemeton <oxydemeton@users.noreply.github.com>
2024-03-19 20:03:08 +0000

    Vite build

--- dist/mod.js
+++ dist/mod.js
@@ -4,15 +4,15 @@ function _(e) {
 function d(e) {
   const n = new Uint8Array(e);
   let t = "";
-  for (const r of n)
-    t += String.fromCharCode(r);
+  for (const o of n)
+    t += String.fromCharCode(o);
   return btoa(t).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 }
 function p(e) {
-  const n = e.replace(/-/g, "+").replace(/_/g, "/"), t = (4 - n.length % 4) % 4, i = n.padEnd(n.length + t, "="), r = atob(i), a = new ArrayBuffer(r.length), o = new Uint8Array(a);
-  for (let u = 0; u < r.length; u++)
-    o[u] = r.charCodeAt(u);
-  return a;
+  const n = e.replace(/-/g, "+").replace(/_/g, "/"), t = (4 - n.length % 4) % 4, a = n.padEnd(n.length + t, "="), o = atob(a), i = new ArrayBuffer(o.length), r = new Uint8Array(i);
+  for (let u = 0; u < o.length; u++)
+    r[u] = o.charCodeAt(u);
+  return i;
 }
 function b() {
   return (window == null ? void 0 : window.PublicKeyCredential) !== void 0 && typeof window.PublicKeyCredential == "function";
@@ -29,12 +29,12 @@ function A(e) {
   return e === "localhost" || /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/i.test(e);
 }
 class s extends Error {
-  constructor({ message: n, code: t, cause: i, name: r }) {
-    super(n, { cause: i }), this.name = r ?? i.name, this.code = t;
+  constructor({ message: n, code: t, cause: a, name: o }) {
+    super(n, { cause: a }), this.name = o ?? a.name, this.code = t;
   }
 }
 function I({ error: e, options: n }) {
-  var i, r;
+  var a, o;
   const { publicKey: t } = n;
   if (!t)
     throw Error("options was missing required publicKey property");
@@ -46,13 +46,13 @@ function I({ error: e, options: n }) {
         cause: e
       });
   } else if (e.name === "ConstraintError") {
-    if (((i = t.authenticatorSelection) == null ? void 0 : i.requireResidentKey) === !0)
+    if (((a = t.authenticatorSelection) == null ? void 0 : a.requireResidentKey) === !0)
       return new s({
         message: "Discoverable credentials were required but no available authenticator supported it",
         code: "ERROR_AUTHENTICATOR_MISSING_DISCOVERABLE_CREDENTIAL_SUPPORT",
         cause: e
       });
-    if (((r = t.authenticatorSelection) == null ? void 0 : r.userVerification) === "required")
+    if (((o = t.authenticatorSelection) == null ? void 0 : o.userVerification) === "required")
       return new s({
         message: "User verification was required but no available authenticator supported it",
         code: "ERROR_AUTHENTICATOR_MISSING_USER_VERIFICATION_SUPPORT",
@@ -72,7 +72,7 @@ function I({ error: e, options: n }) {
         cause: e
       });
     if (e.name === "NotSupportedError")
-      return t.pubKeyCredParams.filter((o) => o.type === "public-key").length === 0 ? new s({
+      return t.pubKeyCredParams.filter((r) => r.type === "public-key").length === 0 ? new s({
         message: 'No entry in pubKeyCredParams was of type "public-key"',
         code: "ERROR_MALFORMED_PUBKEYCREDPARAMS",
         cause: e
@@ -82,9 +82,9 @@ function I({ error: e, options: n }) {
         cause: e
       });
     if (e.name === "SecurityError") {
-      const a = window.location.hostname;
-      if (A(a)) {
-        if (t.rp.id !== a)
+      const i = window.location.hostname;
+      if (A(i)) {
+        if (t.rp.id !== i)
           return new s({
             message: `The RP ID "${t.rp.id}" is invalid for this domain`,
             code: "ERROR_INVALID_RP_ID",
@@ -147,53 +147,53 @@ async function T(e) {
     excludeCredentials: (R = e.excludeCredentials) == null ? void 0 : R.map(E)
   } };
   t.signal = m.createNewAbortSignal();
-  let i;
+  let a;
   try {
-    i = await navigator.credentials.create(t);
+    a = await navigator.credentials.create(t);
   } catch (l) {
     throw I({ error: l, options: t });
   }
-  if (!i)
+  if (!a)
     throw new Error("Registration was not completed");
-  const { id: r, rawId: a, response: o, type: u } = i;
+  const { id: o, rawId: i, response: r, type: u } = a;
   let c;
-  typeof o.getTransports == "function" && (c = o.getTransports());
+  typeof r.getTransports == "function" && (c = r.getTransports());
   let f;
-  if (typeof o.getPublicKeyAlgorithm == "function")
+  if (typeof r.getPublicKeyAlgorithm == "function")
     try {
-      f = o.getPublicKeyAlgorithm();
+      f = r.getPublicKeyAlgorithm();
     } catch (l) {
       g("getPublicKeyAlgorithm()", l);
     }
   let h;
-  if (typeof o.getPublicKey == "function")
+  if (typeof r.getPublicKey == "function")
     try {
-      const l = o.getPublicKey();
+      const l = r.getPublicKey();
       l !== null && (h = d(l));
     } catch (l) {
       g("getPublicKey()", l);
     }
   let w;
-  if (typeof o.getAuthenticatorData == "function")
+  if (typeof r.getAuthenticatorData == "function")
     try {
-      w = d(o.getAuthenticatorData());
+      w = d(r.getAuthenticatorData());
     } catch (l) {
       g("getAuthenticatorData()", l);
     }
   return {
-    id: r,
-    rawId: d(a),
+    id: o,
+    rawId: d(i),
     response: {
-      attestationObject: d(o.attestationObject),
-      clientDataJSON: d(o.clientDataJSON),
+      attestationObject: d(r.attestationObject),
+      clientDataJSON: d(r.clientDataJSON),
       transports: c,
       publicKeyAlgorithm: f,
       publicKey: h,
       authenticatorData: w
     },
     type: u,
-    clientExtensionResults: i.getClientExtensionResults(),
-    authenticatorAttachment: y(i.authenticatorAttachment)
+    clientExtensionResults: a.getClientExtensionResults(),
+    authenticatorAttachment: y(a.authenticatorAttachment)
   };
 }
 function g(e, n) {
@@ -226,9 +226,9 @@ function D({ error: e, options: n }) {
         cause: e
       });
     if (e.name === "SecurityError") {
-      const i = window.location.hostname;
-      if (A(i)) {
-        if (t.rpId !== i)
+      const a = window.location.hostname;
+      if (A(a)) {
+        if (t.rpId !== a)
           return new s({
             message: `The RP ID "${t.rpId}" is invalid for this domain`,
             code: "ERROR_INVALID_RP_ID",
@@ -255,31 +255,31 @@ async function N(e, n = !1) {
     throw new Error("WebAuthn is not supported in this browser");
   let t;
   ((w = e.allowCredentials) == null ? void 0 : w.length) !== 0 && (t = (R = e.allowCredentials) == null ? void 0 : R.map(E));
-  const i = {
+  const a = {
     ...e,
     challenge: p(e.challenge),
     allowCredentials: t
-  }, r = {};
+  }, o = {};
   if (n) {
     if (!await O())
       throw Error("Browser does not support WebAuthn autofill");
     if (document.querySelectorAll("input[autocomplete$='webauthn']").length < 1)
       throw Error('No <input> with "webauthn" as the only or last value in its `autocomplete` attribute was detected');
-    r.mediation = "conditional", i.allowCredentials = [];
+    o.mediation = "conditional", a.allowCredentials = [];
   }
-  r.publicKey = i, r.signal = m.createNewAbortSignal();
-  let a;
+  o.publicKey = a, o.signal = m.createNewAbortSignal();
+  let i;
   try {
-    a = await navigator.credentials.get(r);
+    i = await navigator.credentials.get(o);
   } catch (l) {
-    throw D({ error: l, options: r });
+    throw D({ error: l, options: o });
   }
-  if (!a)
+  if (!i)
     throw new Error("Authentication was not completed");
-  const { id: o, rawId: u, response: c, type: f } = a;
+  const { id: r, rawId: u, response: c, type: f } = i;
   let h;
   return c.userHandle && (h = C(c.userHandle)), {
-    id: o,
+    id: r,
     rawId: d(u),
     response: {
       authenticatorData: d(c.authenticatorData),
@@ -288,53 +288,53 @@ async function N(e, n = !1) {
       userHandle: h
     },
     type: f,
-    clientExtensionResults: a.getClientExtensionResults(),
-    authenticatorAttachment: y(a.authenticatorAttachment)
+    clientExtensionResults: i.getClientExtensionResults(),
+    authenticatorAttachment: y(i.authenticatorAttachment)
   };
 }
 async function K(e, n) {
-  return new Promise((t, i) => {
-    const r = new URL(n);
-    if (!r.protocol.startsWith("ws"))
-      throw new Error(`Invalid protocol for url: ${r}`);
-    r.searchParams.append("username", e);
-    const a = new WebSocket(r);
-    a.onmessage = async (o) => {
-      const u = JSON.parse(o.data);
+  return new Promise((t, a) => {
+    const o = new URL(n);
+    if (!o.protocol.startsWith("ws"))
+      throw new Error(`Invalid protocol for url: ${o}`);
+    o.searchParams.append("username", e);
+    const i = new WebSocket(o);
+    i.onmessage = async (r) => {
+      const u = JSON.parse(r.data);
       let c;
       try {
         c = await T(u);
       } catch (f) {
         throw f;
       }
-      a.send(JSON.stringify(c));
-    }, a.onerror = (o) => {
-      console.log("Ws onerror: ", o);
-    }, a.onclose = (o) => {
-      t(), console.info("Socket closed: ", o.reason);
+      i.send(JSON.stringify(c));
+    }, i.onerror = (r) => {
+      console.log("Ws onerror: ", r);
+    }, i.onclose = (r) => {
+      r.code !== 1e3 ? (console.error("Socket closed with error code: ", r.code, r.reason), a()) : t();
     };
   });
 }
 async function U(e, n) {
-  return new Promise((t, i) => {
-    const r = new URL(n);
-    if (!r.protocol.startsWith("ws"))
-      throw new Error(`Invalid protocol for url: ${r}`);
-    r.searchParams.append("username", e);
-    const a = new WebSocket(r);
-    a.onmessage = async (o) => {
-      const u = JSON.parse(o.data);
+  return new Promise((t, a) => {
+    const o = new URL(n);
+    if (!o.protocol.startsWith("ws"))
+      throw new Error(`Invalid protocol for url: ${o}`);
+    o.searchParams.append("username", e);
+    const i = new WebSocket(o);
+    i.onmessage = async (r) => {
+      const u = JSON.parse(r.data);
       let c;
       try {
         c = await N(u);
       } catch (f) {
         throw f;
       }
-      a.send(JSON.stringify(c));
-    }, a.onerror = (o) => {
-      console.log("Ws onerror: ", o);
-    }, a.onclose = (o) => {
-      t(), console.info("Socket closed: ", o.reason);
+      i.send(JSON.stringify(c));
+    }, i.onerror = (r) => {
+      console.log("Ws onerror: ", r);
+    }, i.onclose = (r) => {
+      r.code !== 1e3 ? (console.error("Socket closed with error code: ", r.code, r.reason), a()) : t();
     };
   });
 }
