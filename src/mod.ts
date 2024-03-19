3b96055bd3fe89babd0c299825b6a95167826c55
M B <mablasteam@gmail.com>
2024-03-19 20:01:46 +0100

    prepare for release

--- src/mod.ts
+++ src/mod.ts
@@ -1,54 +1,60 @@
 import { startAuthentication, startRegistration } from "@simplewebauthn/browser";
 
-export async function registerUser(userName: string, path: URL | string) {
-  const server = new URL(path)
-  if (!server.protocol.startsWith("ws")) throw new Error(`Invalid protocol for url: ${server}`)
-
-  server.searchParams.append("username", userName)
-  const socket = new WebSocket(server)
-  socket.onmessage = async (ev) => {
-    const serverConfig = JSON.parse(ev.data)
-    let attResp;
-    try {
-      // Pass the options to the authenticator and wait for a response
-      attResp = await startRegistration(serverConfig);
-    } catch (error: any) {
-      //TODO: error handling
-      throw error;
+export async function registerUser(userName: string, path: URL | string): Promise<void> {
+  return new Promise<void>((res, _rej)=> {
+    const server = new URL(path)
+    if (!server.protocol.startsWith("ws")) throw new Error(`Invalid protocol for url: ${server}`)
+  
+    server.searchParams.append("username", userName)
+    const socket = new WebSocket(server)
+    socket.onmessage = async (ev) => {
+      const serverConfig = JSON.parse(ev.data)
+      let attResp;
+      try {
+        // Pass the options to the authenticator and wait for a response
+        attResp = await startRegistration(serverConfig);
+      } catch (error: any) {
+        //TODO: error handling
+        throw error;
+      }
+      socket.send(JSON.stringify(attResp))
     }
-    socket.send(JSON.stringify(attResp))
-  }
-  socket.onerror = (ev) => {
-    console.log("Ws onerror: ", ev);
-  }
-  socket.onclose = (ev) => {
-    console.info("Socket closed: ", ev.reason)
-  }
+    socket.onerror = (ev) => {
+      console.log("Ws onerror: ", ev);
+    }
+    socket.onclose = (ev) => {
+      res()
+      console.info("Socket closed: ", ev.reason)
+    }
+  })
 }
 
-export async function loginUser(userName: string, path: URL | string) {
-  const server = new URL(path)
-  if (!server.protocol.startsWith("ws")) throw new Error(`Invalid protocol for url: ${server}`)
-
-  server.searchParams.append("username", userName)
-  const socket = new WebSocket(server)
-
-  socket.onmessage = async (ev) => {
-    const serverConfig = JSON.parse(ev.data)
-    let attResp;
-    try {
-      // Pass the options to the authenticator and wait for a response
-      attResp = await startAuthentication(serverConfig);
-    } catch (error: any) {
-      //TODO: error handling
-      throw error;
+export async function loginUser(userName: string, path: URL | string): Promise<void> {
+  return new Promise<void>((res, _rej) => {
+    const server = new URL(path)
+    if (!server.protocol.startsWith("ws")) throw new Error(`Invalid protocol for url: ${server}`)
+  
+    server.searchParams.append("username", userName)
+    const socket = new WebSocket(server)
+  
+    socket.onmessage = async (ev) => {
+      const serverConfig = JSON.parse(ev.data)
+      let attResp;
+      try {
+        // Pass the options to the authenticator and wait for a response
+        attResp = await startAuthentication(serverConfig);
+      } catch (error: any) {
+        //TODO: error handling
+        throw error;
+      }
+      socket.send(JSON.stringify(attResp))
     }
-    socket.send(JSON.stringify(attResp))
-  }
-  socket.onerror = (ev) => {
-    console.log("Ws onerror: ", ev);
-  }
-  socket.onclose = (ev) => {
-    console.info("Socket closed: ", ev.reason)
-  }
+    socket.onerror = (ev) => {
+      console.log("Ws onerror: ", ev);
+    }
+    socket.onclose = (ev) => {
+      res()
+      console.info("Socket closed: ", ev.reason)
+    }
+  })
 }
