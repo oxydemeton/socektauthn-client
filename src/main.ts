a72d962de95520a0b2f3554179845ebd7688a8d1
M B <mablasteam@gmail.com>
2024-03-20 19:15:23 +0100

    added closing

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ document.getElementById("form")!.addEventListener("submit", (e)=>e.preventDefaul
 document.getElementById("register")!.addEventListener("click", async (e)=> {
   e.preventDefault()
   const userName = (document.getElementById("username")! as HTMLInputElement).value
-  await registerUser(userName, new URL("ws://localhost:5180/register"))
+  const socket = await registerUser(userName, new URL("ws://localhost:5180/register"))
+  socket.close()
 })
 document.getElementById("login")!.addEventListener("click", async (e)=> {
   e.preventDefault()
   const userName = (document.getElementById("username")! as HTMLInputElement).value
-  await loginUser(userName, new URL("ws://localhost:5180/login"))
+  const socket = await loginUser(userName, new URL("ws://localhost:5180/login"))
+  socket.close()
 })
